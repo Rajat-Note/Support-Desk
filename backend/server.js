1c19e7afd0cd9ebaed91b7544acec9c252aafdda
Rajat-Note <95745762+Rajat-Note@users.noreply.github.com>
2023-12-21 22:28:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://support-desk-main.onrender.com/"],
+    origin: ["https://support-desk-main.onrender.com"],
     methods: ["POST", "GET"],
     credentials: true,
   })
